3d32375066744d8a8761698418c3b8de3f9dcc71
Dalton Cézane <daltoncezane@gmail.com>
2018-10-01 15:23:32 -0300

    Added a condition to eliminate secondary studies (survey/review), generating two new files: one with the interested papers and another with the secondary ones for related works analysis.

--- slr_helper.py
+++ slr_helper.py
@@ -1,13 +1,18 @@
 import sys, csv, os, json
 
-
 def main():
-    #TODO add a menu: filter results (input --> output)
+    #TODO add a usage guide: filter results (input --> output)
     #                 remove duplicated entries (input --> output)
+    # --> when excluding reviews/surveys --> get titles for "related works"
     args = sys.argv[1:]
-    filename = str(args[0]) if len(args) != 0 else ""
-    newfilename = str(args[1]) if len(args) > 1 else ""
-    file_exists = os.path.isfile(newfilename)
+    if not len(args) > 2:
+        sys.exit("Usage: python3 slr_helper.py input_file.csv output_file.csv related_works_file.csv")
+
+    in_filename = str(args[0]) # if len(args) != 0 else ""
+    newfilename = str(args[1]) # if len(args) > 1 else ""
+    rw_filename = str(args[2])
+    files = [newfilename, rw_filename]
+    file_exists = [os.path.isfile(newfilename), os.path.isfile(rw_filename)]
 
     header = ["Title", "Authors", "Year", "Abstract", "Keywords"]
 
@@ -18,12 +23,13 @@ def main():
 
     field_names = list()
     field_values = list()
+    relatedworks_values = list()
     for i in range(0, len(header)):
         field_name = input("Enter the exact name of {} field in the file: ".format(header[i]))
         field_names.append(field_name)
 
-    try:    
-        with open(filename, 'r', encoding='utf-8-sig') as infile:
+    try:
+        with open(in_filename, 'r', encoding='utf-8-sig') as infile:
             rowsreader = csv.DictReader(infile, delimiter=",")
             outrowsreader = list()
             if file_exists:
@@ -38,21 +44,27 @@ def main():
 
                 if not any(r["Title"] == dict_row["Title"] for r in outrowsreader) \
                    or not file_exists:
-                    field_values.append(dict_row)
-                    count += 1
+                    if any (s in dict_row["Title"].lower() for s in ["survey", "review"]):
+                        relatedworks_values.append(dict_row)
+                    else:
+                        field_values.append(dict_row)
+                        count += 1
             print(count)
     except IOError:
-        print("Could not open csv file: {}.".format(filename))
+        print("Could not open csv file: {}.".format(in_filename))
 
-    try:
-        with open(newfilename, 'a') as outfile:
-            rowswriter = csv.DictWriter(outfile, header)
-            if not file_exists:
-                rowswriter.writeheader()
-            for row in field_values:
-                rowswriter.writerow(row)
-    except IOError:
-        print("Could not convert to csv file: {}.".format(newfilename))
+    fields = [field_values, relatedworks_values]
+
+    for i in range(0, len(files)):
+        try:
+            with open(files[i], 'a') as outfile:
+                rowswriter = csv.DictWriter(outfile, header)
+                if not file_exists[i]:
+                    rowswriter.writeheader()
+                for row in fields[i]:
+                    rowswriter.writerow(row)
+        except IOError:
+            print("Could not convert to csv file: {}.".format(files[i]))
 
 if __name__ == "__main__":
     main()
